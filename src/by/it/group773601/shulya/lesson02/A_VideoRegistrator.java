261dc5106817eefc759c7987fc828d803e187e78
Pollime <57046129+pollishullya@users.noreply.github.com>
2020-02-05 22:18:51 +0300

    lesson02: A,B,C update

--- src/by/it/group773601/shulya/lesson02/A_VideoRegistrator.java
+++ src/by/it/group773601/shulya/lesson02/A_VideoRegistrator.java
@@ -1,5 +1,6 @@
 package by.it.group773601.shulya.lesson02;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 /*
@@ -20,11 +21,14 @@ public class A_VideoRegistrator {
     }
 
     //модификаторы доступа опущены для возможности тестирования
-    List<Double> calcStartTimes(double[] events, double workDuration) {
+    List<Double> calcStartTimes(double[] events, double workDuration){
         //events - события которые нужно зарегистрировать
         //timeWorkDuration время работы видеокамеры после старта
-        //i - это индекс события events[i]
+        List<Double> result;
+        result = new ArrayList<>();
+        int i=0;                              //i - это индекс события events[i]
         //комментарии от проверочного решения сохранены для подсказки, но вы можете их удалить.
+
         //подготовка к жадному поглощению массива событий
         //hint: сортировка Arrays.sort обеспечит скорость алгоритма
         //C*(n log n) + C1*n = O(n log n)
@@ -35,66 +39,23 @@ public class A_VideoRegistrator {
         //вычислим момент окончания работы видеокамеры
         //и теперь пропустим все покрываемые события
         //за время до конца работы, увеличивая индекс
-        List<Double> eventsList = buildEventsList(events);
-        Collections.sort(eventsList);
-        return calcWorkTime(workDuration, eventsList);//вернем итог
-    }
+        Arrays.sort(events);
 
-    private List<Double> calcWorkTime(double workDuration,
-                                      List<Double> eventsL) {
-        List<Double> result = new ArrayList<>();
-        double timeWork = eventsL.get(0);
-        result.add(timeWork);
-        timeWork += workDuration;
+        double time = events[i]+workDuration;
+        double start = events[i];
+        result.add(events[i]);
 
-        for (Double time : eventsL) {
-            if (time > timeWork) {
-                result.add(time);
-                timeWork = time;
-                timeWork += workDuration;
+        while (i < events.length) {
+            if(events[i] > time) {
+                if(start!=events[i]) {
+                    result.add(events[i]);
+                    start=events[i];
+                }
+                time = start+workDuration;
             }
+            i++;
         }
-        return result;
-    }
 
-    private List<Double> buildEventsList(double[] events) {
-        List<Double> eventsL = new ArrayList<>();
-        for (double eve : events) {
-            eventsL.add(eve);
-        }
-        return eventsL;
+        return result;                        //вернем итог
     }
 }
-
-//public class A_VideoRegistrator {
-//
-//    public static void main(String[] args) {
-//        A_VideoRegistrator instance=new A_VideoRegistrator();
-//        double[] events=new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
-//        List<Double> starts=instance.calcStartTimes(events,1); //рассчитаем моменты старта, с длинной сеанса 1
-//        System.out.println(starts);                            //покажем моменты старта
-//    }
-//    //модификаторы доступа опущены для возможности тестирования
-//    List<Double> calcStartTimes(double[] events, double workDuration){
-//        //events - события которые нужно зарегистрировать
-//        //timeWorkDuration время работы видеокамеры после старта
-//        List<Double> result;
-//        result = new ArrayList<>();
-//        int i=0;                              //i - это индекс события events[i]
-//        //комментарии от проверочного решения сохранены для подсказки, но вы можете их удалить.
-//        //подготовка к жадному поглощению массива событий
-//        //hint: сортировка Arrays.sort обеспечит скорость алгоритма
-//        //C*(n log n) + C1*n = O(n log n)
-//
-//        //пока есть незарегистрированные события
-//        //получим одно событие по левому краю
-//        //и запомним время старта видеокамеры
-//        //вычислим момент окончания работы видеокамеры
-//        //и теперь пропустим все покрываемые события
-//        //за время до конца работы, увеличивая индекс
-//
-//
-//
-//        return result;                        //вернем итог
-//    }
-//}
