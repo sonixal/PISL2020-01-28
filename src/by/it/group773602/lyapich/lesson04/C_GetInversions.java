ca9367aaab80e4a0ae5cf4efb188d0d801865f11
v.lyapich <vladvlad.45@mail.ru>
2020-02-18 11:23:19 +0300

    Lesson4 (A,B,C)

---
+++ src/by/it/group773602/lyapich/lesson04/C_GetInversions.java
@@ -0,0 +1,107 @@
+package by.it.group773602.lyapich.lesson04;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+Рассчитать число инверсий одномерного массива.
+Сложность алгоритма должна быть не хуже, чем O(n log n)
+
+Первая строка содержит число 1<=n<=10000,
+вторая - массив A[1…n], содержащий натуральные числа, не превосходящие 10E9.
+Необходимо посчитать число пар индексов 1<=i<j<n, для которых A[i]>A[j].
+
+    (Такая пара элементов называется инверсией массива.
+    Количество инверсий в массиве является в некотором смысле
+    его мерой неупорядоченности: например, в упорядоченном по неубыванию
+    массиве инверсий нет вообще, а в массиве, упорядоченном по убыванию,
+    инверсию образуют каждые (т.е. любые) два элемента.
+    )
+
+Sample Input:
+5
+2 3 9 2 9
+Sample Output:
+2
+
+Головоломка (т.е. не обязательно).
+Попробуйте обеспечить скорость лучше, чем O(n log n) за счет многопоточности.
+Докажите рост производительности замерами времени.
+Большой тестовый массив можно прочитать свой или сгенерировать его программно.
+*/
+
+
+public class C_GetInversions {
+
+    private int countInversions(int[] inputArray) {
+        int size = inputArray.length;
+
+        if (size < 2) {
+            return 0;
+        }
+
+        int m = (size + 1) / 2;
+        int[] left = Arrays.copyOfRange(inputArray, 0, m);
+        int[] right = Arrays.copyOfRange(inputArray, m, size);
+
+        return countInversions(left) + countInversions(right) + merge(inputArray, left, right);
+    }
+
+    private int merge(int[] arr, int[] left, int[] right) {
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        int leftSize = left.length;
+        int rightSize = right.length;
+
+        while (i < leftSize || j < rightSize) {
+            if (i == leftSize) {
+                arr[i + j] = right[j];
+                j++;
+            } else if (j == rightSize) {
+                arr[i + j] = left[i];
+                i++;
+            } else if (left[i] <= right[j]) {
+                arr[i + j] = left[i];
+                i++;
+            } else {
+                arr[i + j] = right[j];
+                count += leftSize - i;
+                j++;
+            }
+        }
+
+        return count;
+    }
+
+    int calc(InputStream stream) throws FileNotFoundException {
+        //подготовка к чтению данных
+        Scanner scanner = new Scanner(stream);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!
+        //размер массива
+        int n = scanner.nextInt();
+        //сам массив
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!     тут ваше решение   !!!!!!!!!!!!!!!!!!!!!!!!
+        int result = countInversions(a);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return result;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson04/dataC.txt");
+        C_GetInversions instance = new C_GetInversions();
+        //long startTime = System.currentTimeMillis();
+        int result = instance.calc(stream);
+        //long finishTime = System.currentTimeMillis();
+        System.out.print(result);
+    }
+}
