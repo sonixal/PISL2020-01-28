fcf1234125faa8b4b645d82ebd91177fdc9a544c
andreiloiko <andrej_lojko@mail.ru>
2020-03-17 15:27:50 +0300

    lesson5 abc lesson6 abc

--- src/by/it/group773601/loiko/lesson02/A_VideoRegistrator.java
+++ src/by/it/group773601/loiko/lesson02/A_VideoRegistrator.java
@@ -22,20 +22,29 @@ public class A_VideoRegistrator {
     }
     //модификаторы доступа опущены для возможности тестирования
     List<Double> calcStartTimes(double[] events, double workDuration){
-
+        //events - события которые нужно зарегистрировать
+        //timeWorkDuration время работы видеокамеры после старта
         List<Double> result;
         result = new ArrayList<>();
         int i=0;
-
+        //i - это индекс события events[i]
+        //комментарии от проверочного решения сохранены для подсказки, но вы можете их удалить.
+        //подготовка к жадному поглощению массива событий
         Arrays.sort(events);
-
+        //hint: сортировка Arrays.sort обеспечит скорость алгоритма
+        //C*(n log n) + C1*n = O(n log n)
         while (i<events.length-1)    {
             result.add(events[i]);
             Double fin=events[i]+workDuration;
             while(events[i]<fin)
-                i++;                           // C*(n log n) + C1*n = O(n log n)
+                i++;
         }
-
+        //пока есть незарегистрированные события
+        //получим одно событие по левому краю
+        //и запомним время старта видеокамеры
+        //вычислим момент окончания работы видеокамеры
+        //и теперь пропустим все покрываемые события
+        //за время до конца работы, увеличивая индекс
         return result;                        //вернем итог
     }
 }
